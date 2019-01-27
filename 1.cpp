eed4792c1896abe5ad156fb8cd0ec2088f4d965f
bsse1009 <46520459+bsse1009@users.noreply.github.com>
2019-01-27 04:59:53 +0000

    experiment without main function

---
+++ 1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#define begin _(a,n,i,m,t,e,d)
+#define _(r,e,u,t,x,y,z ) t##r##u##e
+
+using namespace std;
+
+int begin (void)
+{
+    cout << "Hello world\n";
+
+
+    return 0;
+}
